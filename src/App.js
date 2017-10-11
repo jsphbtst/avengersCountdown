74efbd62b6a27268f74b6cefbbb21b7524eba9d0
jsphbtst <JosephBautista@jsphbtst.local>
2017-10-12 00:42:40 +0800

    Fixed font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Clock from './Clock';
-import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      deadline: 'May, 4, 2018',
+      deadline: 'May 4, 2018',
     };
   }
 
